934ca9a5fb15937e9c0f888ab5254f4396313ec9
CodeYouness <ylijassi@gmail.com>
2024-08-01 00:43:17 +0200

    added create-or-edit

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -1,53 +1,17 @@
-@extends('layouts.admin')
+@extends('admin.projects.layouts.create-or-edit')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12">
-            <form action="{{ route('admin.projects.store') }}" method="POST">
-                @method("POST")
-                @csrf
-
-                <div class="mb-3">
-                    <h2>
-                        Create new post
-                    </h2>
-                </div>
-
-                <div class="mb-3">
-                    <label for="title">Title:</label>
-                    <input type="text" name="title" id="title" class="form-control mb-3"
-                    value="{{ old('title', $project->title)}}">
-                    @error("title")
-                        <div class="alert alert-danger mb-3">
-                            {{$message}}
-                        </div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label for="image">Image url:</label>
-                    <input type="text" name="image" id="image" class="form-control mb-3"
-                    value="{{ old('image', $project->image)}}">
-                    @error("image")
-                        <div class="alert alert-danger mb-3">
-                            {{$message}}
-                        </div>
-                    @enderror
-                </div>
-                <div class="mb-3">
-                    <label for="content">Content:</label>
-                    <textarea type="text" name="content" id="content" class="form-control" rows="10" class="form-control">{{ old('content', $project->content)}}</textarea>
-                    @error("content")
-                        <div class="alert alert-danger my-3">
-                            {{$message}}
-                        </div>
-                    @enderror
-                </div>
-
-                <input type="submit" value="Create new post" class="btn btn-primary btn-lg">
-                <input type="reset" value="Reset Fields" class="btn btn-warning btn-lg">
-            </form>
-        </div>
-    </div>
-</div>
+@section('page-title')
+    Create new post
 @endsection
+
+
+@section('form-action')
+    {{route('admin.projects.store')}}
+@endsection
+
+@section('form-method')
+    @method("POST")
+@endsection
+
+
+{{route('admin.projects.store')}}
