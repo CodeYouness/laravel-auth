a28b4efcf40521f84a05d891d782717b9445942e
CodeYouness <ylijassi@gmail.com>
2024-07-31 17:51:12 +0200

    added post to index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,7 +3,29 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        @dump($projects)
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <td>{{$project->id}}</td>
+                            <td>{{$project->author}}</td>
+                            <td>{{$project->title}}</td>
+                            <td>{{$project->date}}</td>
+                            <td><a href="" class="btn btn-primary">Show</a></td>
+                        </tr>
+                    @endforeach
+            </tbody>
+        </table>
+
     </div>
 </div>
 @endsection
